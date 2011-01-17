bc8c701abd13f934471d2dd4f447ac767c89614d
Matthew Brett <matthew.brett@gmail.com>
2011-01-17 10:54:41 -0800

    BF - relax decimal threshold for closeness

--- dipy/external/tests/test_fsl.py
+++ dipy/external/tests/test_fsl.py
@@ -24,20 +24,20 @@ ONE_VOL = nib.Nifti1Image(_small_img.get_data()[...,0],
 def test_flirt_behavior():
     # self to self is identity
     res0 = fsl.flirt(ONE_VOL, ONE_VOL)
-    assert_array_almost_equal(res0, np.eye(4))
+    assert_array_almost_equal(res0, np.eye(4), decimal=2)
     # pos determinant version of same image
     pos_det = nib.as_closest_canonical(ONE_VOL)
     # original to pos det version has x flip
     res1 = fsl.flirt(ONE_VOL, pos_det)
     flipx = np.diag([-1,1,1,1])
     flipx[0,3] = ONE_VOL.get_shape()[0]-1
-    assert_array_almost_equal(res1, flipx)
+    assert_array_almost_equal(res1, flipx, decimal=2)
     # Either way round
     res2 = fsl.flirt(pos_det, ONE_VOL)
-    assert_array_almost_equal(res2, flipx)
+    assert_array_almost_equal(res2, flipx, decimal=2)
     # pos det to self obviously also identity
     res3 = fsl.flirt(pos_det, pos_det)
-    assert_array_almost_equal(res3, np.eye(4))
+    assert_array_almost_equal(res3, np.eye(4), decimal=2)
     # Mapping is from ``in`` to ``ref`` voxels
     arr = ONE_VOL.get_data()
     arr = arr[:,:,3:]
@@ -48,5 +48,6 @@ def test_flirt_behavior():
     assert_array_almost_equal(res4, [[1,0,0,0],
                                      [0,1,0,0],
                                      [0,0,1,3],
-                                     [0,0,0,1]])
+                                     [0,0,0,1]],
+                             decimal=1)
 
